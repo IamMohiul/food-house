58a4c23f1bb5f37e56f18db080be0b1c2f175b53
IamMohiul <m2mohiul@gmail.com>
2023-12-14 02:57:42 +0600

    025714122023

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+        /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::insert([
+            [
+                'name'=>'Admin',
+                'email'=>'admin@gmail.com',
+                'role'=>'admin',
+                'password' =>static::$password ??= Hash::make('password'),
+            ],
+            [
+                'name'=>'User',
+                'email'=>'user@gmail.com',
+                'role'=>'user',
+                'password' =>static::$password ??= Hash::make('password'),
+            ]
+            ]);
+    }
+}
---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+      Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Developed By <a href="https://github.com/IamMohiul">Mohiul Alam</a>
+    </div>
+    <div class="footer-right">
+
+    </div>
+  </footer>
